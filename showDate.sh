0b7a322893dd0d911489d14e68cb61d206b02a04
keepitfoxy <lisiecka.dev@gmail.com>
2024-04-20 22:52:25 +0200

    seventh

--- showDate.sh
+++ showDate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#show date
+# Show date
 show_date() {
     echo "Dzisiejsza data: $(date)"
 }
 
-#create logs
+# Create logs
 logs() {
     local num_logs=${1:-100} # Ustawienie domyślnej wartości na 100, jeśli nie podano argumentu
     for ((i=1; i<=num_logs; i++)); do
@@ -13,21 +13,32 @@ logs() {
     done
 }
 
-# show help
+# Show help
 show_help() {
     echo "Użycie: $0 [opcja]"
     echo "Opcje:"
     echo "  --date, -d  : Wyświetla dzisiejszą datę."
     echo "  --logs, -l [N] : Tworzy N logów. Domyślnie tworzy 100 logów, jeśli nie podano liczby N."
     echo "  --help, -h     : Wyświetla tę pomoc."
+    echo "  --init         : Inicjuje skrypt, klonując repozytorium do bieżącego katalogu i ustawiając ścieżkę w PATH."
 }
 
+#init
+initialize_script() {
+    git clone https://github.com/keepitfoxy/KL-Lab4.git .
+
+    export PATH="$(pwd):$PATH"
+}
+
+# Main logic
 if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     show_date
 elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     logs "$2"
 elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     show_help
+elif [[ "$1" == "--init" ]]; then
+    initialize_script
 else
     logs
 fi
