317e89ba4c5025b3b0116dd549ac73fb9daf88e4
keepitfoxy <lisiecka.dev@gmail.com>
2024-04-20 22:12:57 +0200

    fourth

--- showDate.sh
+++ showDate.sh
@@ -7,13 +7,16 @@ show_date() {
 
 #create logs
 logs() {
-    for ((i=1; i<=100; i++)); do
+    local num_logs=${1:-100} # Ustawienie domyślnej wartości na 100, jeśli nie podano argumentu
+    for ((i=1; i<=num_logs; i++)); do
         echo "log$i.txt, created by $0 on $(date)" > "log$i.txt"
     done
 }
 
 if [[ "$1" == "--date" ]]; then
     show_date
+elif [[ "$1" == "--logs" ]]; then
+    logs "$2"
 else
     logs
 fi
