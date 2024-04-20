dc5db09789e68f85b465ed8d1009dc6fe6991bfb
keepitfoxy <lisiecka.dev@gmail.com>
2024-04-20 22:02:57 +0200

    third

--- showDate.sh
+++ showDate.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+#show date
 show_date() {
     echo "Dzisiejsza data: $(date)"
 }
 
-show_date
+#create logs
+logs() {
+    for ((i=1; i<=100; i++)); do
+        echo "log$i.txt, created by $0 on $(date)" > "log$i.txt"
+    done
+}
+
+if [[ "$1" == "--date" ]]; then
+    show_date
+else
+    logs
+fi
